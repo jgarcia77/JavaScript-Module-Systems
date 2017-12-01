d38628e3036292f1a2383222d51efbe2c357dfb2
jgarcia77 <jgarcia773@gmail.com>
2017-12-01 17:56:44 -0600

    The Revealing Module Patter

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 console.log('app loaded!');
+
+var dogModule = (function() {
+    function bark(name) {
+        console.log(name + ' is barking');
+    }
+
+    // Reveal bark through public pointer speak
+    return {
+        speak: bark
+    };
+})();
+
+var catModule = (function() {
+    function meow(name) {
+        console.log(name + ' is meowing');
+    }
+
+    // Reveal meow through public pointer speak
+    return {
+        speak: meow
+    };
+})();
+
+// invoke speak for each module
+dogModule.speak('Brie');
+catModule.speak('Kittie');
+
+// inject module dependencies
+var petOwner = (function(dogModule, catModule){
+    dogModule.speak('Lightening');
+    catModule.speak('Pussy');
+})(dogModule, catModule);
