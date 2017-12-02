20e6b7044286e412dcf9423cea5bd4f681207b5c
jgarcia77 <jgarcia773@gmail.com>
2017-12-01 19:36:46 -0600

    AMD

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 console.log('app loaded!');
+
+require(['./counter.js'], function(counter) {
+    console.log(counter.getValue());
+    counter.increment();
+    console.log(counter.getValue());
+    counter.decrement();
+    console.log(counter.getValue());
+
+    counter.increment();
+    counter.increment();
+    counter.increment();
+    console.log(counter.getValue());
+});
